0ddc05f506311f56d71b3d4be8164726b3352caf
wahidabd <wahed.blog99@gmail.com>
2022-11-04 00:26:24 +0700

    fix: send mail on server

--- config/mail.php
+++ config/mail.php
@@ -38,12 +38,12 @@ return [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
-            'verify_peer' => false
+            'verify_peer' => false,
         ],
 
         'ses' => [
@@ -115,5 +115,4 @@ return [
             resource_path('views/vendor/mail'),
         ],
     ],
-
 ];
