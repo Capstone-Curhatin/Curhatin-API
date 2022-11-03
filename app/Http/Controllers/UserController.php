e4aaf2fdf9bfa001c9700bbb86b8a91eb05146d3
wahidabd <wahed.blog99@gmail.com>
2022-11-03 18:13:40 +0700

    fix: login request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,54 +2,87 @@
 
 namespace App\Http\Controllers;
 
-use App\Helpers\ResponseFormatter;
-use App\Models\User;
 use Exception;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Fortify\Rules\Password;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Laravel\Fortify\Rules\Password;
 
 class UserController extends Controller {
 
     public function login(Request $request) {
+
         try {
             $validate = Validator::make($request->all(), [
-               'email' => ['required', 'email'],
-               'password' => ['required', 'password']
+                'email' => ['required', 'email'],
+                'password' => 'required'
             ]);
 
-            if ($validate->fails()) {
-                return ResponseFormatter::error([
-                    $validate->errors()
-                ], 'Authentication Failed');
-            }
-
-            $credentials = request(['email', 'password']);
-            if (!Auth::attempt($credentials)) {
-                return ResponseFormatter::error([
-                    'message' => 'Unauthorized',
-                ], 'Login Failed');
+            if ($validate->fails()){
+                return ResponseFormatter::error(null, $validate->errors()->all());
             }
 
             $user = User::where('email', $request->email)->first();
-            if(!Hash::check($request->password, $user->password, [])) {
+
+            if(!$user){
+                return ResponseFormatter::error( null, 'Email not found');
+            }else if(!Hash::check($request->password, $user->password)){
+                return ResponseFormatter::error(null, 'Invalid password');
+            }
+
+            if (!Hash::check($request->password, $user->password, [])) {
                 throw new \Exception('Invalid Credentials');
             }
 
-            $token = $user->creatToken('authToken')->plainTextToken;
+            $token = $user->createToken('authToken')->plainTextToken;
             return ResponseFormatter::success([
                 'access_token' => $token,
                 'token_type' => 'Bearer',
                 'user' => $user
             ], 'Authenticated');
-        }catch (\Exception $err) {
+
+        } catch (Exception $err) {
             return ResponseFormatter::error([
                 'message' => 'Something went wrong',
                 'error' => $err,
             ], 'Authentication Failed');
         }
+
+//        try {
+//            $validate = Validator::make($request->all(), [
+//               'email' => ['required', 'email'],
+//               'password' => ['required', 'password']
+//            ]);
+//
+//            if ($validate->fails()) {
+//                return ResponseFormatter::error(null, $validate->errors()->all());
+//            }
+//
+//
+//            return ResponseFormatter::success("oke", "masok");
+//
+////            $credentials = request(['email', 'password']);
+////            if(!Auth::attempt($credentials)){
+////                return ResponseFormatter::error(null, 'Login Failed');
+////            }
+////
+////            $user = User::where('email', $request->email)->first();
+////            if (!Hash::check($request->password, $user->password)) {
+////                throw new \Exception('Invalid Credentials');
+////            }
+////
+////            $token = $user->createToken('authToken')->plainTextToken;
+////            return ResponseFormatter::success([
+////                'access_token' => $token,
+////                'token_type' => 'Bearer',
+////                'user' => $user
+////            ], 'Authenticated');
+//        }catch (\Exception $err) {
+//            return ResponseFormatter::error(null, 'Authentication Failed');
+//        }
     }
 
     public function register(Request $request) {
@@ -78,7 +111,7 @@ class UserController extends Controller {
             return ResponseFormatter::success([], 'User Registered');
 
         }catch(Exception $err){
-            return ResponseFormatter::error([],$err);
+            return ResponseFormatter::error(null, $err);
         }
     }
 
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -15,8 +14,8 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
 
 Route::middleware(['auth:sanctum'])->group(function (){
 
