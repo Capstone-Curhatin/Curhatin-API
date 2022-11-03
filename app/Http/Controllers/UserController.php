bcace6cfa9e8800b378cdf8cd15fa62f8ae8d2d9
wahidabd <wahed.blog99@gmail.com>
2022-11-03 14:33:14 +0700

    fix: error handling in register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Helpers\ResponseFormatter;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Rules\Password;
 
 class UserController extends Controller {
 
@@ -56,28 +58,27 @@ class UserController extends Controller {
             $validate = Validator::make($request->all(), [
                 'name' => ['required', 'string', 'max:30'],
                 'email' => ['required', 'email', 'max:50', 'unique:users'],
+                'phone' => ['required', 'string', 'max:15'],
+                'role' => ['required', 'integer', 'max:1'],
                 'password' => ['required', 'string', 'min:6', new Password]
             ]);
 
             if ($validate->fails()){
-                return ResponseFormatter::error([
-                    $validate->errors()
-                ], 'Authentication Failed');
+                return ResponseFormatter::error(null, $validate->errors()->all());
             }
 
             User::create([
                 'name' => $request->name,
                 'email' => $request->email,
+                'phone' => $request->phone,
+                'role' => $request->role,
                 'password' => Hash::make($request->password)
             ]);
 
             return ResponseFormatter::success([], 'User Registered');
 
-        }catch(\Exception $err){
-            return ResponseFormatter::error([
-                'message' => 'Something went wrong',
-                'error' => $err,
-            ],'Authentication Failed');
+        }catch(Exception $err){
+            return ResponseFormatter::error([],$err);
         }
     }
 
